546b4ca6df5447f9803148b5fc7e5862f85df0bd
billy <bgrayd@gmail.com>
2016-09-27 17:38:10 -0500

    refactored navigator and made unit tests

--- cr17/test/test_navigator.py
+++ cr17/test/test_navigator.py
@@ -6,12 +6,84 @@ import unittest
 import rostest
 
 from cr17 import navigator
+from geometry_msgs.msg import Point, Pose
+
 
 
 class TestNavigator(unittest.TestCase):
+    def assertListAlmostEqual(self, list1, list2, tol = 7):
+        self.assertEqual(len(list1), len(list2))
+        for a, b in zip(list1, list2):
+             self.assertAlmostEqual(a, b, tol)
 
-    def test_one_equals_one(self):
-        self.assertEquals(1, 1)
+    def test_at_goal(self):
+        samePose = Point()
+        differentPose = Point()
+        robotPose = Pose()
+
+        samePose.x = 1
+        samePose.y = 12
+
+        differentPose.x = 12
+        differentPose.y = 1
+
+        robotPose.position.x = 1
+        robotPose.position.y = 12
+
+        self.assertTrue(navigator.at_goal(robotPose, samePose))
+        self.assertFalse(navigator.at_goal(robotPose, differentPose))
+
+    def test_calc_goal_force(self):
+        goalForward = Point()
+        goalBackward = Point()
+        goalLeft = Point()
+        goalRight = Point()
+        goalFrontLeft = Point()
+        goalFrontRight = Point()
+        goalBackLeft = Point()
+        goalBackRight = Point()
+
+        goalForward.x = 1
+        goalForward.y = 0
+
+        goalBackward.x = -1
+        goalBackward.y = 0
+
+        goalLeft.x = 0
+        goalLeft.y = 1
+
+        goalRight.x = 0
+        goalRight.y = -1
+
+        goalFrontLeft.x = 1
+        goalFrontLeft.y = 1
+
+        goalFrontRight.x = 1
+        goalFrontRight.y = -1
+
+        goalBackLeft.x = -1
+        goalBackLeft.y = 1
+
+        goalBackRight.x = -1
+        goalBackRight.y = -1
+
+        robotPose = Pose()
+        robotPose.position.x = 0
+        robotPose.position.y = 0
+
+        self.assertListAlmostEqual([0.9,0], navigator.calc_goal_force(goalForward, robotPose))
+        self.assertListAlmostEqual([-0.9,0], navigator.calc_goal_force(goalBackward, robotPose))
+        self.assertListAlmostEqual([0,0.9], navigator.calc_goal_force(goalLeft, robotPose))
+        self.assertListAlmostEqual([0,-0.9], navigator.calc_goal_force(goalRight, robotPose))
+        self.assertListAlmostEqual([0.929,0.929], navigator.calc_goal_force(goalFrontLeft, robotPose), 3)
+        self.assertListAlmostEqual([0.929,-0.929], navigator.calc_goal_force(goalFrontRight, robotPose), 3)
+        self.assertListAlmostEqual([-0.929,0.929], navigator.calc_goal_force(goalBackLeft, robotPose), 3)
+        self.assertListAlmostEqual([-0.929,-0.929], navigator.calc_goal_force(goalBackRight, robotPose), 3)
+
+    def test_calc_repulsive_force(self):
+        self.assertTrue(True) #TODO: fill out
+
+
 
 if __name__ == '__main__':
     rostest.unitrun(PKG, NAME, TestNavigator)
