b4d75ff6433f6decc9a5a8e5fd3682f9b05d1c67
Jacob Mason <jacob@jacobmason.net>
2016-10-01 21:36:08 -0500

    Adds basic sub/pub to obstactle-mapper

--- cr17/nodes/cr17/obstacle_mapper.py
+++ cr17/nodes/cr17/obstacle_mapper.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python2
 import rospy
 
+from std_msgs.msg import Header
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import PointCloud2
+from nav_msgs.msg import MapMetaData, OccupancyGrid
+
+
 class ObstacleMapper(object):
     def __init__(self):
         rospy.init_node('obstacle_mapper')
+        rospy.Subscriber('robot_pose', Pose, self.robot_pose_cb)
+        rospy.Subscriber('obstacles', PointCloud2, self.obstacles_cb)
+        self.robot_pose = Pose
+        self.obstacles = PointCloud2
+
+        self.pub = rospy.Publisher('map', OccupancyGrid, latch=True, queue_size=5)
+
+    def robot_pose_cb(self, data):
+        rospy.loginfo("Got robot_pose:\n%s", data)
+        self.robot_pose = data
+
+    def obstacles_cb(self, data):
+        rospy.loginfo("Got obstacles:\n%s", data)
+        self.obstacles = data
 
     def run(self):
-        rospy.spin()
-
-
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            h = Header()
+            h.stamp = rospy.Time.now()
+            self.pub.publish(OccupancyGrid(header=h))
+            rate.sleep()
 
 if __name__ == "__main__":
-	obstacleMapper = ObstacleMapper()
-	obstacleMapper.run()
+    obstacleMapper = ObstacleMapper()
+    obstacleMapper.run()
