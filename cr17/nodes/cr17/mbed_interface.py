b322810c7f2fd7422eb1b2bd286e933e34683a2d
JoshuaBRussell <theruss007@gmail.com>
2016-10-10 23:27:41 -0500

    Able to send Twist params and Scoop params over USB

--- cr17/nodes/cr17/mbed_interface.py
+++ cr17/nodes/cr17/mbed_interface.py
@@ -17,9 +17,24 @@ Sends low-level commands(scoop, arm, linear velocity, angular velocity) to Mbed 
 Also accepts speed values from Mbed for each wheel, and publishes to the wheel_speed topic. 
 '''
 
+##Mbed Send Package Description(8 bytes)
+#0 CMD_Vel: Z-Angular
+#1 CMD_Vel: X-inear
+#2 Scoop  : Arm-Angular
+#3 Scoop  : Scoop-Angular
+#4 Unused :
+#5 Unused :
+#6 Unused :
+#7 Unused :
+
 ROS_SLEEP_RATE = 10
+
 MBED_VENDOR_ID = 0x1234
 MBED_PRODUCT_ID = 0x0006
+
+# 4 bits for fraction + 3 bits for integer + 1 bit for sign = 8 bits!!! ()
+FRAC_BIT_WIDTH = 4
+INT_BIT_WIDTH = 3
 ######################################
 #Topic Variables
 ######################################
@@ -27,9 +42,6 @@ DRIVE_TOPIC = "/cmd_vel"
 SCOOP_TOPIC = "/cmd_scoop"   
 WHEEL_SPEED_TOPIC = "/wheel_speed"
 
-# handler called when a report is received
-def rx_handler(data):
-    print 'recv: ', data
 
 class MbedInterface(object):
     def __init__(self):
@@ -39,6 +51,10 @@ class MbedInterface(object):
         self.__cmd_vel = Twist()
         self.__cmd_scoop = scoopControl()
         self.__wheel_data = wheelData()
+
+        #Raw data list(byte array?) to/from the Mbed
+        self.__data = [0x0] * 8  #Initializes bytes to be set to zero
+        self.__bytes = None
         
         ######################################
         # Setup ROS publishers
@@ -73,34 +89,53 @@ class MbedInterface(object):
             self.__endpoint = self.__hid_device[0][(0,0)][0]
 
 
+
+    ######################################
+    # ROS Callbacks
+    ######################################
+
     #Add new cmd_vel to USB message(x-linear/z-angular)
-    def cmd_vel_callback(self, data):
+    def cmd_vel_callback(self, twist_msg):
         print "Received Twist from ", DRIVE_TOPIC
+        self.__data[0] = self.float_to_fixed_point(twist_msg.angular.z)
+        self.__data[1] = self.float_to_fixed_point(twist_msg.linear.x)
 
     #Add new scoop cmd to USB message(arm velocity/scoop velocity)
-    def cmd_scoop_callback(self, data):
+    def cmd_scoop_callback(self, scoop_msg):
         print "Received scoopControl from ", SCOOP_TOPIC
+        self.__data[2] = self.float_to_fixed_point(scoop_msg.armVelAngular)
+        self.__data[3] = self.float_to_fixed_point(scoop_msg.scoopVelAngular)
 
     #Sends data recieved from Mbed over a topic(wheelData)
     def mbed_recieve_handler(self, data):
         print "Mbed Recv: ", data
+
+    ######################################
+    # Needed Functions
+    ######################################
+
+    #Converts float to fixed point(represented as an 8 byte int) based of the scheme described in the wiki.
+    def float_to_fixed_point(self, float_val):
+        fixed_val = int(round(float_val * 2**FRAC_BIT_WIDTH))
+
+        return fixed_val
+
+
+    #Converts fixed point(represented as an 8 byte int) based of the scheme described in the wiki.
+    def fixed_point_to_float(self, data):
+        print "Stuff"
 
 
 
     def run(self):
         while not rospy.is_shutdown():
             rate = rospy.Rate(ROS_SLEEP_RATE)
-            data = [0x0] * 8
                         
             #read the data
-            bytes = self.__hid_device.read(self.__endpoint.bEndpointAddress, 8, timeout=5000)
-            self.mbed_recieve_handler(bytes)
+            self.__bytes = self.__hid_device.read(self.__endpoint.bEndpointAddress, 8, timeout=5000)
+            self.mbed_recieve_handler(self.__bytes)
         
-            for i in range(8):
-                data[i] = bytes[i]
-            
-
-            self.__hid_device.write(1, data)
+            self.__hid_device.write(1, self.__data)
         
             rate.sleep()
 
