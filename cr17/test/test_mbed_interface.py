fc94f6a7e995752774897b05662ea6a8e7322608
JoshuaBRussell <theruss007@gmail.com>
2016-10-20 22:18:32 -0500

    Updated files for testing

--- cr17/test/test_mbed_interface.py
+++ cr17/test/test_mbed_interface.py
@@ -8,6 +8,9 @@ import rostest
 
 from cr17 import mbed_interface
 
+SINGLE_BYTE_UPPER_LIMIT = 7.9
+SINGLE_BYTE_LOWER_LIMIT = -7.9
+
 
 class TestMbedInterface(unittest.TestCase):
 
@@ -20,6 +23,9 @@ class TestMbedInterface(unittest.TestCase):
 	#Test for fixed_to_float
 
 	#Test for float_to_fixed
+	def test_upper_limit_rejection(self):
+		self.assertRaises(ValueError, mbed_interface.float_to_fixed, 8.0)
+
 
 
 if __name__ == '__main__':
