a08a4110291243b9e9a8d828bda1935ccaa155c5
nathantfrank <nathan.t.frank@gmail.com>
2017-02-19 16:55:31 -0600

    Added publisher, subscriber, and service

--- cr17/nodes/cr17/scoop_controller.py
+++ cr17/nodes/cr17/scoop_controller.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python2
 import rospy
 from std_msgs.msg import Int16
+from cr17.msg import scoopControl
+from cr17.srv import autonomousActive
+
+ARM_STATE_TOPIC = rospy.get_param("topics/scoop_state_cmds", "scoop_commands")
+
+ARM_GOAL_TOPIC = rospy.get_param("topics/scoop_cmds", "cmd_sccop")
 
 
 class ScoopController(object):
     def __init__(self):
         rospy.init_node('scoop_controller')
 
-        self.autonomy = True
+        # Services
+        self.autonomy = rospy.Service('autonomousScoop', autonomousActive, self.set_autonomy)
+
+        # Publishers
+        self.scoop_pub = rospy.Publisher(ARM_GOAL_TOPIC, Int16, queue_size=10)
+
+        # Subscribers
+        self.arm_sub = rospy.Subscriber(ARM_STATE_TOPIC, scoopControl, queue_size=10)
 
     def run(self):
         rospy.spin()
 
-    def set_autonomy(self, autonomous):
-        self.autonomy = autonomous
-
+    def set_autonomy(self, req):
+        self.autonomy = req.autonomousActive
+        return True
 
 if __name__ == "__main__":
     scoopController = ScoopController()
