fcbe2c718a13b857e622369879c9379f5f6b34b3
nathantfrank <nathan.t.frank@gmail.com>
2017-02-19 15:22:24 -0600

    incremental update

--- cr17/nodes/cr17/scoop_controller.py
+++ cr17/nodes/cr17/scoop_controller.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python2
 import rospy
+from std_msgs.msg import Int16
+
 
 class ScoopController(object):
     def __init__(self):
         rospy.init_node('scoop_controller')
 
+        self.autonomy = True
+
     def run(self):
         rospy.spin()
 
+    def set_autonomy(self, autonomous):
+        self.autonomy = autonomous
 
 
 if __name__ == "__main__":
-	scoopController = ScoopController()
-	scoopController.run()
+    scoopController = ScoopController()
+    scoopController.run()
