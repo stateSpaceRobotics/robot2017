8e2560a5cff824a4becb18ebf9e534955e9a543b
billy <bgrayd@gmail.com>
2017-03-03 14:13:27 -0600

    Added service and renamed functions

--- cr17/nodes/cr17/high_level_state_controller.py
+++ cr17/nodes/cr17/high_level_state_controller.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import PoseStamped, Twist, Pose
 from std_msgs.msg import Float64, Bool
 from nav_msgs.msg import Path
 from cr17.msgs import scoopControl
+from cr17.srv import autonomousActive, autonomousActiveResponse
 
 
 ARM_STATE_TOPIC = rospy.get_param("topics/scoop_state_cmds", "scoop_commands")
@@ -35,12 +36,7 @@ class high_level_state_controller(object):
     def __init__(self):
         rospy.init_node("high_level_state_controller")
 
-        self.arm_pub = rospy.Publisher(ARM_STATE_TOPIC, scoopControl, queue_size=10)
-        self.goal_pub = rospy.Publisher(GOAL_TOPIC, PoseStamped, queue_size=10) #TODO: change to a Path
-
-        rospy.Subscriber(POSE_TOPIC, PoseStamped, self.pose_sub)
         self.pose = Pose()
-        rospy.Subscriber(PATH_TOPIC, Path, self.path_sub)
         self.pathPoses = []     #TODO: maybe remove, send entire current path to navigator
         self.curPathIndex = 0   #TODO: maybe remove, send entire current path to navigator
         self.autostate = "INIT"
@@ -51,6 +47,22 @@ class high_level_state_controller(object):
         self.miningDone = False
         self.minePath = None
         self.dumpTimer = None
+        self.autonomyEnabled = True
+
+        # ROS Publishers
+        self.arm_pub = rospy.Publisher(ARM_STATE_TOPIC, scoopControl, queue_size=10)
+        self.goal_pub = rospy.Publisher(GOAL_TOPIC, PoseStamped, queue_size=10) #TODO: change to a Path
+
+        #ROS Subscribers
+        rospy.Subscriber(POSE_TOPIC, PoseStamped, self.pose_sub)
+        rospy.Subscriber(PATH_TOPIC, Path, self.path_sub)
+
+        # ROS Services
+        self.auto_srv = rospy.Service('autonomousHLSC', autonomousActive, self.set_autonomy)
+
+    def set_autonomy(self, data):
+        self.autonomyEnabled = data.autonomousActive
+        return autonomousActiveResponse(True)
 
     def pose_sub(self, data):
         self.pose = data.pose
@@ -58,42 +70,42 @@ class high_level_state_controller(object):
     def path_sub(self, data):
         self.pathPoses = data.poses
 
-    def armDriveState(self):
+    def arm_drive_state(self):
         newMsg = scoopControl()
         newMsg.desiredState = scoopControl.state_drivingPosition
         self.arm_pub.publish(newMsg)
 
-    def armPreDigState(self):
+    def arm_predig_state(self):
         newMsg = scoopControl()
         newMsg.desiredState = scoopControl.state_preDigging
         self.arm_pub.publish(newMsg)
 
-    def armDigState(self):
+    def arm_dig_state(self):
         newMsg = scoopControl()
         newMsg.desiredState = scoopControl.state_digging
         self.arm_pub.publish(newMsg)
 
-    def armPostDigState(self):
+    def arm_postdig_state(self):
         newMsg = scoopControl()
         newMsg.desiredState = scoopControl.state_postDigging
         self.arm_pub.publish(newMsg)
 
-    def armPreDumpState(self):
+    def arm_predump_state(self):
         newMsg = scoopControl()
         newMsg.desiredState = scoopControl.state_preDump
         self.arm_pub.publish(newMsg)
 
-    def armDumpState(self):
+    def arm_dump_state(self):
         newMsg = scoopControl()
         newMsg.desiredState = scoopControl.state_dump
         self.arm_pub.publish(newMsg)
 
-    def armPostDumpState(self):
+    def arm_postdump_state(self):
         newMsg = scoopControl()
         newMsg.desiredState = scoopControl.state_postDump
         self.arm_pub.publish(newMsg)
 
-    def calcMiningPath(self, angle):
+    def calculate_mining_path(self, angle):
         start_x = self.pose.position.x
         start_y = self.pose.position.y
 
@@ -111,7 +123,7 @@ class high_level_state_controller(object):
         path.append(endPose)
         return path
 
-    def closeTo(self, poseChecked):
+    def close_to(self, poseChecked):
         try:
             x_dist = abs(self.pose.position.x - poseChecked.position.x)
             y_dist = abs(self.pose.position.y - poseChecked.position.y)
@@ -120,7 +132,7 @@ class high_level_state_controller(object):
             y_dist = abs(self.pose.position.y - poseChecked.pose.position.y)
 
         euclid_dist = x_dist + y_dist
-        print("In closeTo")
+        print("In close_to")
         return (euclid_dist < IS_CLOSE_DIST)
 
     def run(self):
@@ -137,66 +149,65 @@ class high_level_state_controller(object):
 
             elif(self.autostate == "F_OBSTACLE_FIELD"):
                 if(len(self.pathPoses) != 0):
-                    if(self.closeTo(self.pathPoses[self.curPathIndex])):
+                    if(self.close_to(self.pathPoses[self.curPathIndex])):
                         self.curPathIndex += 1
                         if(self.curPathIndex >= len(self.pathPoses)):
                             self.curPathIndex = len(self.pathPoses) - 1
                     self.goal_pub.publish(self.pathPoses[self.curPathIndex])
-                self.armDriveState()
+                self.arm_drive_state()
 
             elif(self.autostate == "MINING_BEHAVIOR"):
                 #first prepare to mine (so the scoop is going down while the robot is slowly driving forward)
                 if not self.miningReady:
                     self.miningReady = True
-                    self.armPreDigState() #TODO: consider waiting a number of iterations
+                    self.arm_predig_state() #TODO: consider waiting a number of iterations
 
                 #keep a counter of the number of the times it has already mined, can create path based on angle
                 #   pulled from an array based on iteration, make sure to reset the mining path to None when leaving this state
                 elif(self.miningReady and not self.miningDone ):
-                    self.armDigState()
+                    self.arm_dig_state()
 
                     if(self.minePath == None):
-                        self.minePath = self.calcMiningPath(ANGLES_TO_MINE[self.miningAngleIndex])
+                        self.minePath = self.calculate_mining_path(ANGLES_TO_MINE[self.miningAngleIndex])
                         self.miningAngleIndex += 1
                         if(self.miningAngleIndex >= len(ANGLES_TO_MINE)):
                             self.miningAngleIndex = 0
 
-                    if(self.closeTo(self.minePath[self.miningPathIndex])):
+                    if(self.close_to(self.minePath[self.miningPathIndex])):
                         self.miningPathIndex += 1
                         if (self.miningPathIndex >= len(self.minePath)):
                             self.minePath = len(self.minePath) - 1
                     self.goal_pub.publish(self.minePath[self.miningPathIndex])
 
-                    if(self.closeTo(self.minePath[len(self.minePath) - 1])):
+                    if(self.close_to(self.minePath[len(self.minePath) - 1])):
                         self.miningDone = True
-                        self.armPostDigState()
+                        self.arm_postdig_state()
 
                 #once the end of the path has been reached, close the hand and iterate back across path
                 elif self.miningDone:
-                    self.armPostDigState()
-                    if (self.closeTo(self.minePath[self.miningPathIndex])):
+                    self.arm_postdig_state()
+                    if (self.close_to(self.minePath[self.miningPathIndex])):
                         self.miningPathIndex -= 1
                         if (self.miningPathIndex < 0):
                             self.minePath = 0
                     self.goal_pub(self.minePath[self.miningPathIndex])
 
                 #once the beginning has been reached again
-                mining_complete = True #TODO: remove and have the autonomy state skip mining when in teleop, and just don't change back during it
 
             ######################################################################################################
             #TODO: add code to turn the robot around, probably removing the mining done section of mining state
             ######################################################################################################
 
             elif(self.autostate == "B_OBSTACLE_FIELD"):
-                self.armDriveState()
-                if(self.closeTo(self.pathPoses[self.curPathIndex])):
+                self.arm_drive_state()
+                if(self.close_to(self.pathPoses[self.curPathIndex])):
                     self.curPathIndex -= 1
                     if(self.curPathIndex < 0 ):
                         self.curPathIndex = 0
                 self.goal_pub.publish(self.pathPoses[self.curPathIndex])
 
             elif(self.autostate == "DOCKING"):
-                self.armPreDumpState()
+                self.arm_predump_state()
                 dockPose = PoseStamped()
                 dockPose.pose.position.x = X_POS_DUMP
                 dockPose.pose.position.y = Y_POS_DUMP
@@ -207,9 +218,9 @@ class high_level_state_controller(object):
                     self.dumpTimer = rospy.Time.now() + rospy.Duration(TIME_DUMP_SEC)
                 elif(self.dumpTimer >= rospy.Time.now() ):
                     dumping_complete = True
-                    self.armPostDumpState()
+                    self.arm_postdump_state()
                 else:
-                    self.armDumpState()
+                    self.arm_dump_state()
 
             ######################################################################################################
             #TODO: add code to turn the robot around
@@ -223,7 +234,7 @@ class high_level_state_controller(object):
                 if(self.pose.position.y >= Y_IN_MINING_AREA):
                     self.autostate = "MINING_BEHAVIOR"
             elif(self.autostate == "MINING_BEHAVIOR"):
-                if(mining_complete or (self.pose.position.y < Y_IN_MINING_AREA - .3)): #TODO: change the mining_complete to a teleop on check
+                if(not self.autonomyEnabled or (self.pose.position.y < Y_IN_MINING_AREA - .3)):
                     self.autostate = "B_OBSTACLE_FIELD"
                     self.minePath = None
             elif(self.autostate == "B_OBSTACLE_FIELD"):
