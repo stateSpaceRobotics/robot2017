73e69018ca23640bee35b5e3c731bc660d27928c
nathantfrank <nathan.t.frank@gmail.com>
2017-04-08 10:54:02 -0500

    removed duplicate code and added imports for Beacon and LaserObject

--- cr17/nodes/cr17/beacon_localization.deprecated.py
+++ cr17/nodes/cr17/beacon_localization.deprecated.py
@@ -5,6 +5,8 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped, Point, Quaternion
 from tf.transformations import quaternion_from_euler
 from std_msgs.msg import Bool
+from beacon import Beacon
+from laser_object import LaserObject
 
 '''
 Proof of concept/sandbox module for trying out different ways to process lidar data.
@@ -23,51 +25,6 @@ LARGE_NUMBER = 9999999  # Arbitrarily large number
 #LEFT_POST_LOC = (0.65, 0)     # Global coordinate of left post
 #RIGHT_POST_LOC = (-0.65, 0)    # Global coordinate of right post
 #####################################
-
-class LaserObject(object):
-    '''
-    Data structure used to hold objects detected in laser scans 
-    '''
-    def __init__(self):
-        self.left_edge  = None  # index of left edge
-        self.right_edge = None  # index of right edge
-        self.length     = None  # obj length
-        self.centroid   = None  # index of object centroid
-        self.distance   = None  # centroid range in scan
-        self.angle      = None  # centroid angle in scan
-
-    def process(self, scan_msg):
-        '''
-        Given scan_msg and left and right edges, calculate:
-         - length
-         - centroid 
-         - distance 
-         - angle 
-        '''
-        assert self.left_edge != None and self.right_edge != None, "Object edges not set.  Cannot process."
-        arc_ang = (self.left_edge - self.right_edge) * scan_msg.angle_increment
-        right_dist = scan_msg.ranges[self.right_edge]
-        left_dist = scan_msg.ranges[self.left_edge]
-        try:
-            self.length = math.sqrt((right_dist**2 + left_dist**2) - (2 * right_dist * left_dist * math.cos(arc_ang)))
-        except:
-            self.length = 0
-        self.centroid = int(abs(self.right_edge - self.left_edge) / 2) + self.right_edge
-        try:
-            self.distance = scan_msg.ranges[self.centroid]
-        except:
-            self.distance = 0
-        self.angle = self.centroid * scan_msg.angle_increment
-
-class Beacon(object):
-    '''
-    Data structure used to store information on beacon found in laser scan 
-    '''
-    def __init__(self, right_post, left_post, actual_dist, err):
-        self.right_post = right_post    # LaserObject that stores right post
-        self.left_post  = left_post     # LaserObject that stores left post
-        self.actual_dist = actual_dist  # Actual distance between right and left post
-        self.err = err                  # (POST_DIST) - |DIST(right_post, left_post)|
 
 
 class BeaconLocalizer(object):
